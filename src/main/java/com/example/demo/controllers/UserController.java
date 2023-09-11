0746370d4005a7da9e124a94c6065b0f2903f4c1
mk <6177242+anagoxson@users.noreply.github.com>
2023-09-12 00:36:45 +0900

    mod

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -3,7 +3,7 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.app.repository.UserRepository;
+import com.example.demo.repository.UserRepository;
 
 @RestController
 public class UserController {
